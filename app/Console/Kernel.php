3ab634e6d8e5e21e5899a00da1c981a10f34fbf8
Muhammad Abdullah <abd07142011@gmail.com>
2022-08-28 19:39:55 -0400

    test cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('auto:SendMail')->timezone('America/New_York')->dailyAt('19:30')->runInBackground();
+        $schedule->command('auto:SendMail')->timezone('America/New_York')->dailyAt('19:45')->runInBackground()->emailOutputTo('anskam1008@gmail.com');
     }
 
     /**
