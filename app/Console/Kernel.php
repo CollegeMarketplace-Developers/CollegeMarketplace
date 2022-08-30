d6cd3be5ed2eb3d603f5bafd98aba3fa4b9ab7a2
Muhammad Abdullah <abd07142011@gmail.com>
2022-08-30 18:09:21 -0400

    fix time to send emails

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         //setting the command to run at this time 
         $filePath = '/var/log/job1.log';
-        $schedule->command('auto:SendMail')->timezone('America/New_York')->dailyAt('19:52')->runInBackground()->appendOutputTo($filePath);
+        $schedule->command('auto:SendMail')->timezone('America/New_York')->dailyAt('03:00')->runInBackground()->appendOutputTo($filePath);
     }
 
     /**
