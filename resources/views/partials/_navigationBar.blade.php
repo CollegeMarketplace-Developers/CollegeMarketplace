98f860a3bce60afa1558ce807c4b8f7ea4c91635
David Rodas <drodas012@gmail.com>
2022-09-15 01:12:20 -0400

    seperated post section in navbar and cleaned up some comments

--- resources/views/partials/_navigationBar.blade.php
+++ resources/views/partials/_navigationBar.blade.php
@@ -49,8 +49,10 @@
                     </ul>
                 </li>
 
-                {{-- button to sell --}}
-                <li>
+
+                
+                {{-- Old Post with Drop Down for Options --}}
+                <!-- <li>
                     <a class="desktop-link">Post</a>
                     <input type="checkbox" id="show-services">
 
@@ -62,9 +64,25 @@
                     <li><a href="/rentables/create">For Rent</a></li>
                     <li><a href="/subleases/create">For Lease</a></li>
                     </ul>
-                </li>
-                {{-- button to requets --}}
+                </li> -->
+
+
+
+                {{-- button to sell --}}
+                <li><a class="desktop-link" href="/listings/create">Sell</a></li>
+
+
+                {{-- button to rent --}}
+                <li><a class="desktop-link" href="/rentables/create">Rent</a></li>
+
+
+                {{-- button to lease --}}
+                <li><a class="desktop-link" href="/subleases/create">Lease</a></li>
+
+
+                {{-- button to request --}}
                 <li><a href="#">Requests</a></li>
+
 
                 {{-- @auth
                     <li>
