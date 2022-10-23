e45874e924aa45161e71b4e38d524e86f7fc7d06
Muhammad Abdullah <abd07142011@gmail.com>
2022-10-23 17:35:16 -0400

    delete cron logs + change schedule + fix bug

--- app/Console/Commands/SendEmailsHourly.php
+++ app/Console/Commands/SendEmailsHourly.php
@@ -44,17 +44,17 @@ class SendEmailsHourly extends Command
                 ->groupBy('messages.from','messages.to','listings.id','listings.user_id')
                 ->orderBy('listings.id')->distinct()->get();
 
-                $unreadRentals = Message::join('listings','messages.to','=','rentables.user_id')
+                $unreadRentals = Message::join('rentables','messages.to','=','rentables.user_id')
                 ->join('users','users.id','=','messages.from')
                 ->selectRaw('count(*) as numMessages, rentables.id as rentableID, rentables.rental_title as itemName, users.id as senderID, users.first_name as senderFirstName, users.last_name as senderLastName')
                 ->where('is_read','=','0')->where('rentables.user_id','=',$user->id)
                 ->where('is_email','=','0')->groupBy('messages.from','messages.to','rentables.id','rentables.user_id')
                 ->orderBy('rentables.id')->distinct()->get();
 
-                $unreadLease = Message::join('listings','messages.to','=','subleases.user_id')
+                $unreadLease = Message::join('subleases','messages.to','=','subleases.user_id')
                 ->join('users','users.id','=','messages.from')
                 ->selectRaw('count(*) as numMessages, subleases.id as listingID, subleases.sublease_title as itemName, users.id as senderID, users.first_name as senderFirstName, users.last_name as senderLastName')
-                ->where('is_read','=','0')->where('listings.user_id','=',$user->id)
+                ->where('is_read','=','0')->where('subleases.user_id','=',$user->id)
                 ->where('is_email','=','0')->groupBy('messages.from','messages.to','subleases.id','subleases.user_id')
                 ->orderBy('subleases.id')->distinct()->get();
 
