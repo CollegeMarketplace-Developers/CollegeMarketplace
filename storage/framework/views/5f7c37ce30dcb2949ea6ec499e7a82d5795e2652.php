98a1608193d5ce1f51a6ba13b4738ac75ae654d5
Muhammad Abdullah <abd07142011@gmail.com>
2022-12-04 11:41:39 -0500

    run

--- storage/framework/views/5f7c37ce30dcb2949ea6ec499e7a82d5795e2652.php
+++
@@ -1,8 +0,0 @@
-<?php if(session()->has('message')): ?>
-    <div x-data="{show: true}" x-init="setTimeout(() => show = false, 5000)" x-show="show" class="flash-message">
-        <p>
-            <?php echo e(session('message')); ?>
-
-        </p>
-    </div>
-<?php endif; ?><?php /**PATH C:\xampp\htdocs\CollegeMarketplace\resources\views/components/flash-message.blade.php ENDPATH**/ ?>
