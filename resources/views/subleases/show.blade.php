dc4f5cbb291cf29d6e8f3d5ada7bbc7c71e81d77
Muhammad Abdullah <abd07142011@gmail.com>
2022-12-16 02:47:08 -0500

    load more for recent

--- resources/views/subleases/show.blade.php
+++ resources/views/subleases/show.blade.php
@@ -254,9 +254,15 @@
                                     @endif
                                 </ul>
 
-                                <div id = "input-text" class=input-text>
-                                    <input type="text" name="message" placeholder="Message Seller" class="submit">
-                                </div>
+                                @if(auth()->guest()) 
+                                    <div id = "input-text" class=input-text>
+                                        <input type="text" name="message" placeholder="Please Login to begin chat." class="submit" disabled>
+                                    </div>
+                                @else
+                                    <div id = "input-text" class=input-text>
+                                        <input type="text" name="message" placeholder="Message Seller" class="submit">
+                                    </div>   
+                                @endif
                             </div> 
                         </div>
                     </div>
